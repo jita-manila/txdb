6ae26477721d2a03752c41669e07331ac8c44823
贾茹 <11518761+jia-ru-if@user.noreply.gitee.com>
2022-10-30 02:20:25 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div class="content">
+      <router-view />
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped>
+#app {
+  background: url("assets/bg.jpeg") no-repeat center top;
+  padding: 16px 16px 50px;
+  min-height: 100vh;
+  background-size: cover;
+}
+
+.content {
+  max-width: 480px;
+  margin: 0 auto;
+}
+</style>
